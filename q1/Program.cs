cce4b86f2ca4df17bb5e68747fffe8f6b8556434
dananjaya2002 <kavishandananjaya2002@gmail.com>
2024-02-01 19:52:52 +0530

    change

--- q1/Program.cs
+++ q1/Program.cs
@@ -10,8 +10,43 @@ namespace q1
     {
         static void Main()
         {
-            Console.WriteLine("hello");
-            
+            Console.WriteLine("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Mark: ");
+            int mark = int.Parse(Console.ReadLine());
+
+            string grade;
+
+            if (mark >= 100)
+            {
+                Console.WriteLine("Wrong Marks");
+                grade = "error";
+            }
+            else if (mark >= 75)
+            {
+                grade = "A";
+            }
+            else if (mark >= 60)
+            {
+                grade = "B";
+            }
+            else if (mark >= 50)
+            {
+                grade = "C";
+            }
+            else if (mark >= 40)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "Failed";
+            }
+
+            Console.WriteLine(name + " your Grade is " + grade);
+            Console.ReadLine();
+
         }
 
         
