e9bc5dbef210ad74cdf754eef4f7d040b88c84ae
dananjaya2002 <kavishandananjaya2002@gmail.com>
2024-02-01 12:51:54 +0530

    change

--- q1/Program.cs
+++ q1/Program.cs
@@ -10,28 +10,11 @@ namespace q1
     {
         static void Main()
         {
-            Console.WriteLine("Enter the number of Fibonacci term:");
-            int n = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Fibonacci number:");
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write(Fibonacci(i) + " ");
-            }
-            Console.ReadLine();
+            Console.WriteLine("hello");
+            
         }
 
-        static int Fibonacci(int n)
-        {
-            if (n <= 1)
-            {
-                return n;
-            }
-            else
-            {
-                return Fibonacci(n - 1) + Fibonacci(n - 2);
-            }
-        }
+        
 
     }
 }
