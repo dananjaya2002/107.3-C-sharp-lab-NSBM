1a0bee851ac0c4bae41b8e242223937e33de6801
dananjaya2002 <kavishandananjaya2002@gmail.com>
2024-02-01 19:49:54 +0530

    change

--- q1/Program.cs
+++ q1/Program.cs
@@ -8,13 +8,54 @@ namespace q1
 {
     internal class Program
     {
-        static void Main()
+
+        static void Bal()
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("your balance is xxxxx.");
+        }
+
+        static void Dep() 
+        { 
+            Console.WriteLine("Enter the money through counter thank you."); 
+        }
+
+        static void Widraw() 
+        { 
+            Console.WriteLine("Enter the Amount you want to withdraw");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + " amout is ready to withdarw");
             
         }
 
-        
+        static void Main()
+        {
+            Console.WriteLine("How do you want to proceed?");
+            Console.WriteLine("type '1' for check the balance");
+            Console.WriteLine("type '2' for deposit money");
+            Console.WriteLine("type '3' for withdraw money");
+            int num = int.Parse(Console.ReadLine());
 
+            if(num == 1)
+            {
+                Bal();
+            }
+            else if(num == 2)
+            {
+                Dep();
+            }
+            else if (num == 3)
+            {
+                Widraw();
+            }
+            else
+            {
+                Console.WriteLine("Invalid command");
+            }
+
+            Console.ReadLine();
+
+        }
+
+        
     }
 }
