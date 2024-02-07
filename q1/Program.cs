c53e8015634b868ffdc6959a6c92e44bab54871a
dananjaya2002 <kavishandananjaya2002@gmail.com>
2024-02-07 12:18:57 +0530

    change

--- q1/Program.cs
+++ q1/Program.cs
@@ -1,29 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace q1
 {
-    //create a BankAccount class    
-    public class BankAccout
+    //create a Shape class    
+    public class Shape
     {
-        private int bal = 1000;
+        private double area;
+        private string shapeType;
 
+        //public property for shape type
+
+        public string ShapeType { get { return shapeType; }  }
+
+        public Shape(string shapeType)
+        {
+            this.shapeType = shapeType;
+        }
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Shape Type: {shapeType}");
+            Console.WriteLine($"Area: {area}");
+        }
         
-        public void Check_balance()
-        {
-            Console.WriteLine($"Your account balance is: {bal}");
-        }
-
-        public void Deposit( int change)
-        {
-            bal = bal + change;
-            Console.WriteLine($"{change} amount is deposited to your account");
-            Console.WriteLine($"Your new balance is {bal}");
-        }
-
+        
 
     }
 
@@ -33,32 +38,8 @@ namespace q1
         
         static void Main()
         {
-            //create an object
-            BankAccout account1 = new BankAccout();
-
-            Console.WriteLine("type '1' for check Balance or type '2' for deposit amount");
-            int choice = int.Parse( Console.ReadLine() );
-
-            if ( choice == 1)
-            {
-                //calling the checkBalance method for checking the balance
-                account1.Check_balance();
-            }
-            else if ( choice == 2)
-            {
-                //calling the deposit method for Deposit the amount
-                Console.WriteLine("Enter the amount you wish to deposit");
-                int amount = int.Parse( Console.ReadLine() );
-
-                account1.Deposit(amount);
-            }
-            else
-            {
-                Console.WriteLine("Invalid option");
-            }
-
+           
             
-            Console.ReadLine();
         }
 
         
