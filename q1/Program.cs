fa39900b17807f6161cefd8342110c4b84840cff
dananjaya2002 <kavishandananjaya2002@gmail.com>
2024-02-14 15:51:03 +0530

    change

--- q1/Program.cs
+++ q1/Program.cs
@@ -10,28 +10,52 @@ namespace q1
     {
         private int EmployeeID;
         private string FullName;
-        private double Salary;
+        public double Salary;
 
-        public Employee(int employeeID, string fullName, double salary)
+        public Employee(int employeeID, string name, double salary)
         {
             EmployeeID = employeeID;
-            FullName = fullName;
+            FullName = name;
             Salary = salary;
         }
 
         
         public int employeeID => EmployeeID;//read-only property for employeeID
 
+        public string fullName
+        {
+            get
+            {
+                return FullName;
+            }
+            set
+            {
+                FullName = value;
+            }
+        }
+
         public void DisplayEmployeeInfo()
         {
             Console.WriteLine($"Employee's ID: {employeeID}");
+            Console.WriteLine($"Employee's Name: {FullName}");
+            Console.WriteLine($"Employee's Salary: {Salary}");
         }
+
+        
     }
     internal class Program
     {
        public static void Main(string[] args)
         {
-            
+            Employee emp1 = new Employee(101, "John Doe", 50000);
+            Console.WriteLine("Enter the new Name of the Employee: ");
+            emp1.fullName = Console.ReadLine();
+
+            Console.WriteLine("Enter the new Salary of the Emplyee: ");
+            emp1.Salary = double.Parse(Console.ReadLine());
+
+            emp1.DisplayEmployeeInfo();
+
 
 
 
